63acf238298143d38b09c5715628f67357ae6977
hollowcodes <teddypeifer@gmail.com>
2021-12-20 13:10:47 +0100

    added name and role signup fields, moved signup validation server side

--- src/LoginBox.js
+++ src/LoginBox.js
@@ -15,6 +15,8 @@ export default class LoginBox extends React.Component {
 
         this.state = {
             authState: this.props.authState,
+            name: "",
+            role: "none",
             loginEmail: "",
             loginPassword: "",
             signupEmail: "",
@@ -39,12 +41,16 @@ export default class LoginBox extends React.Component {
         this.colorChoiceBar(this.state.authState);
     }
 
-    createFieldErrorStyle(className, placeholderErrorText, placeholderActualText) {
+    createFieldErrorStyle(className, placeholderErrorText, placeholderActualText, isSelection) {
         var inputField = document.getElementsByClassName(className)[0];
-        inputField.value = "";
-        inputField.placeholder = placeholderErrorText;
         inputField.style.borderColor = "red";
         inputField.style.boxShadow = "1px 2px 4px rgba(255, 0, 0, 0.25)";
+        
+        // it this function is called to make the role-selecion field error, don't replace text 
+        if (!isSelection) {
+            inputField.placeholder = placeholderErrorText;
+            inputField.value = "";
+        }
 
         inputField.addEventListener("mouseup", function(e) {
             inputField.placeholder = placeholderActualText;
@@ -65,15 +71,15 @@ export default class LoginBox extends React.Component {
         return (
             <div className="loginSectionBox">
 
-                <label className="emailLabel">email</label>
+                <label className="emailLabel loginEmailLabel">email</label>
                 <input type="text" placeholder="enter your email" value={this.state.loginEmail} onChange={(e) => {
                     this.setState({loginEmail: e.target.value})
-                }} className="loginEmailField"/>
+                }} className="emailField loginEmailField"/>
 
-                <label className="passwordLabel">password</label>
+                <label className="passwordLabel loginPasswordLabel">password</label>
                 <input type="password" placeholder="enter your password" value={this.state.loginPassword} onChange={(e) => {
                     this.setState({loginPassword: e.target.value})
-                }} className="loginPasswordField"/>
+                }} className="passwordField loginPasswordField"/>
 
                 <button className="submitAuthButton" onClick={(e) => {
                     if (this.state.loginEmail.length < 3) {
@@ -114,7 +120,6 @@ export default class LoginBox extends React.Component {
                 }}>log in</button>
 
                 <div className="authButtonShadow"></div>
-
             </div>
         );
     }
@@ -122,38 +127,90 @@ export default class LoginBox extends React.Component {
     createSignupBox() {
         return (
             <div className="signupSectionBox">
-                <label className="emailLabel">email</label>
+
+                <label className="nameLabel">full name</label>
+                <input type="text" placeholder="first and last name" value={this.state.name} onChange={(e) => {
+                    this.setState({name: e.target.value})
+                }} className="nameField"/>
+
+                <label className="roleLabel">role</label>
+                <select name="role" onChange={(e) => {
+                    this.setState({role: e.target.value});
+                    var roleField = document.getElementsByClassName("roleField")[0];
+                    roleField.classList.add("roleSelected");
+
+                    if (e.target.value == "none") {
+                        roleField.classList.remove("roleSelected");
+                    }
+                }}class="roleField">
+                    <option value="none" class="roleOption">select role</option>
+                    <option value="researcher" class="roleOption">researcher</option>
+                    <option value="student" class="roleOption">student</option>
+                    <option value="else" class="roleOption">else</option>
+                </select>
+                
+                <label className="emailLabel signupEmailLabel">email</label>
                 <input type="text" placeholder="example@mail.com" value={this.state.signupEmail} onChange={(e) => {
                     this.setState({signupEmail: e.target.value})
-                }} className="signupEmailField"/>
+                }} className="emailField signupEmailField"/>
 
-                <label className="passwordLabel">password</label>
-                <input type="password" placeholder="min 10 characters, at least 1 number" value={this.state.signupPassword} onChange={(e) => {
+                <label className="passwordLabel signupPasswordLabel">password</label>
+                <input type="password" placeholder="minimum of 10 characters, at least 1 number" value={this.state.signupPassword} onChange={(e) => {
                     this.setState({signupPassword: e.target.value})
-                }} className="signupPasswordField"/>
+                }} className="passwordField signupPasswordField"/>
 
                 <label className="repeatPasswordLabel">repeat password</label>
-                <input type="password" placeholder="min 10 characters, at least 1 number" value={this.state.repeatedPassword} onChange={(e) => {
+                <input type="password" placeholder="minimum of 10 characters, at least 1 number" value={this.state.repeatedPassword} onChange={(e) => {
                     this.setState({repeatedPassword: e.target.value})
                 }} className="repeatPasswordField"/>
 
                 <button className="submitAuthButton" onClick={() => {
-                    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    
+                    /* potential client side validations:
 
+                    if (this.state.role == "none") {
+                        this.createFieldErrorStyle("roleField", null, null, true);
+                        return null;
+                    }
+                    if (this.state.name.length > 40) {
+                        this.createFieldErrorStyle("nameField", "please shorten name!", "first and last name");
+                        return null;
+                    }
+
+                    var latinOnly = true;
+                    var matchStr = this.state.name.match(/[a-zA-Z ]+/);
+                    if (matchStr == null) {
+                        latinOnly = false;
+                    }
+                    latinOnly = matchStr[0].length == this.state.name.length;
+                    if(!latinOnly) {                
+                        this.createFieldErrorStyle("nameField", "latin characters only!", "first and last name");
+                        return null;
+                    }
+                    if (this.state.name.length < 3) {
+                        this.createFieldErrorStyle("nameField", "please provide full name!", "first and last name");
+                        return null;
+                    }
+
+                    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                     if (!re.test(String(this.state.signupEmail).toLowerCase())) {
-                        this.createFieldErrorStyle("signupEmailField", "please enter a valid email address!", "example@mail.com");
+                        this.createFieldErrorStyle("loginEmailField", "please enter a valid email address!", "example@mail.com");
                         return null;
                     }
-                    else if (this.state.signupPassword.length < 10 || !/\d/.test(this.state.signupPassword)) {
-                        this.createFieldErrorStyle("signupPasswordField", "min 10 characters, at least 1 number!", "min 10 characters, at least 1 number");
+                    if (this.state.signupPassword.length < 10 || this.state.signupPassword.length > 63 || !/\d/.test(this.state.signupPassword)) {
+                        this.createFieldErrorStyle("signupPasswordField", "minimum of 10 characters, at least 1 number!", "minimum of 10 characters, at least 1 number");
                         return null;
                     }
-                    else if (this.state.signupPassword !== this.state.repeatedPassword) {
-                        this.createFieldErrorStyle("repeatPasswordField", "this doesn't match your password!", "min 10 characters, at least 1 number");
+                    */
+
+                    if (this.state.signupPassword !== this.state.repeatedPassword) {
+                        this.createFieldErrorStyle("repeatPasswordField", "the passwords don't match!", "minimum of 10 characters, at least 1 number");
                         return null;
                     }
 
                     var data = {
+                        name: this.state.name,
+                        role: this.state.role,
                         email: this.state.signupEmail,
                         password: this.state.signupPassword,
                     }
@@ -167,9 +224,31 @@ export default class LoginBox extends React.Component {
                             }, 5000);
 
                         }, (error) => {
-                            if(error.response.data.error === "emailDuplicateExists") {
+                            if (error.response.data.error === "invalidSignupNameTooShort") {
+                                this.createFieldErrorStyle("nameField", "please provide full name!", "first and last name");
+                            }
+                            else if (error.response.data.error === "invalidSignupNameTooLong") {
+                                this.createFieldErrorStyle("nameField", "please shorten name!", "first and last name");
+                            }
+                            else if (error.response.data.error === "invalidSignupNameNotLatin") {
+                                this.createFieldErrorStyle("nameField", "latin characters only!", "first and last name");
+                            }
+                            else if (error.response.data.error === "invalidSignupRole") {
+                                this.createFieldErrorStyle("roleField", null, null, true);
+                            }
+                            else if (error.response.data.error === "invalidSignupEmail") {
+                                console.log("no")
+                                this.createFieldErrorStyle("signupEmailField", "please enter a valid email address!", "example@mail.com");
+                            }
+                            else if (error.response.data.error === "emailDuplicateExists") {
                                 this.createFieldErrorStyle("signupEmailField", "this email does already exist!", "example@mail.com");
-                            };
+                            }
+                            else if (error.response.data.error === "invalidSignupPassword") {
+                                this.createFieldErrorStyle("signupPasswordField", "minimum of 10 characters, at least 1 number!", "minimum of 10 characters, at least 1 number");
+                            }
+                            else {
+                                ;
+                            }
                         });
 
                 }}>sign up</button>
